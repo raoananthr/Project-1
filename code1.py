055207649ea03295c129a95772265b90b6a2b057
raoananthr <47028022+raoananthr@users.noreply.github.com>
2020-10-23 17:25:20 +0530

    Aim

--- code1.py
+++ code1.py
@@ -1,1 +1,5 @@
+###This Project takes the input of marks, handwriting and other parameters.
+### output should give us the amount of tilt of the student towards an individual's intelligence to 8 types and gives them a score in each one.
+###  Also the output contains what is ypur learning type out of VARK (Visual, Auditaoty, Reading and writing,Kinesthetic)
+### This can be done by NLP, few types of tests(multi intelligence tests)
 
